75a267454eb0e7ce15daf6d9ef57e6df0b6e6773
rileyap <80732437+rileyap@users.noreply.github.com>
2022-05-25 21:16:42 -0400

    Initial commit

--- main_file.py
+++ main_file.py
@@ -1,1 +1,15 @@
+from openpyxl import load_workbook
+path = ""
+names =  ["name", "name2"]
+endFile = ""
+for name in names:
+    continue
+actualFileName = path + endFile
+
+wb = load_workbook(filename= actualFileName)
+sheet = wb.active
+
+sheet["A1"] = "l"
+
+wb.save(filename= actualFileName)
 
