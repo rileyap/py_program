856ece5d7ae0ec7c30e9e4b81fac3f04a8187cf0
rileyap <80732437+rileyap@users.noreply.github.com>
2022-05-30 15:02:23 -0400

    Added functionality to be able to check todays date against pay period date

--- main_file.py
+++ main_file.py
@@ -1,4 +1,6 @@
 from openpyxl import load_workbook
+from datetime import date, datetime, time
+from datetime import timedelta
 path = "C:\\git_workspaces\\py_program\\"
 names =  ["RP", "JG","AK"]
 print("Enter your intials capitalized:")
@@ -11,8 +13,12 @@ actualFileName = path + endFile
 
 wb = load_workbook(filename= actualFileName)
 sheet = wb.active
-
+today = date.today()
 sheet["A1"] = "l"
-
+dateFromExcel = sheet["E1"].value
+dateFromExcel = dateFromExcel.date()
+print(dateFromExcel)
+diff_date = today - dateFromExcel
+print(diff_date)
 wb.save(filename= actualFileName)
 
