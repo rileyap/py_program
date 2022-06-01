8fdfb3649e7006d17dce4f5d81efde66f17d27b4
rileyap <80732437+rileyap@users.noreply.github.com>
2022-06-01 19:02:39 -0400

    Created function to get shift length

--- main_file.py
+++ main_file.py
@@ -8,11 +8,15 @@ nameInput = input()
 endFile = ""
 choosenKey = ""
 shiftWorked = ""
+hoursWorked = ""
+startShift = ""
+endShift = ""
 dict = {'RP': 'PEARSON, RILEY', 'JG': 'GONZALEZ, JOSH', 'OA' :'ALBERS, OLIVIA', 'AB': 'BUBP, AUDRA', 'AF': 'FULLENKAMP, ALLYSEN', 
 'CF': 'FULLENKAMP, CARSON', 'PG': 'GUGGENBILLER, PAIGE', 'KH': 'HEITKAMP, KAYLA', 'LH': 'HIPPLE, LAURA', 'JK': 'JOELLE, KAUP',
 'ABK': 'KNAPKE, ABIGAIL', 'ALK': 'KNAPKE, ALLISON', 'IK': 'KNAPKE, ISAAC', 'NN': 'NGUYEN, NGOC', 'AV': 'VAUGHN, ALLISON', 
 'FW': 'WENDEL, FAITH' }
 dict2 = {'PEARSON, RILEY': 'prompt', 'GONZALEZ, JOSH': 'prompt', 'VAUGHN, ALLISON': 'prompt'}
+dict3 = {'G': 'N', 'g': 'N', 'L': 'O', 'l': 'O', 'M': 'P', 'm': 'P'}
 for key in dict:
     if key == nameInput:
         endFile = dict[key] + ".xlsx"
@@ -26,6 +30,26 @@ else:
         if key == choosenKey:
             print("Enter your type of shift: M for managment, G for guard, L for lesson")
             shiftWorked = input()
+if shiftWorked == "":
+    print("Enter your type of shift: G for guard, L for Lesson")
+    shiftWorked = input()
+
+for key in dict3:
+    if shiftWorked == key:
+        shiftWorked = dict3[key]
+        print(shiftWorked)
+print("Enter your shift length: D for day, N for night, C for custom")
+hoursWorked = input()
+if hoursWorked == 'm' or 'M':
+    print("Enter the start of your shift: example 12:00 PM")
+    startShift = input()
+    print("Enter the end of your shift: example 3:00 PM")
+    endShift = input()
+print(startShift, endShift)
+
+
+
+
 
 
 wb = load_workbook(filename= actualFileName)
